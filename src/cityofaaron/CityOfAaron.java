00494b44c2c0563dce35d7de38e7623545de06cb
Fernando Hernandez <her16028@byui.edu>
2018-10-09 11:36:49 -0700

    First Setup
    This commit includes the creation of the project and the new repository

---
+++ src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Sara Compoverde, Annika Rau, Fernando Hernandez 
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author fernando
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

bd153ca06bba6e7f6fb9f2a94d06bbcc42112fff
Fernando Hernandez <her16028@byui.edu>
2018-10-24 15:02:05 -0700

    W06 - Team Assigment
    Created the control class for the cropData and the buyLand function.

--- src/control/CropControl.java
+++ src/control/CropControl.java
@@ -12,11 +12,12 @@ import java.util.Random;
  *
  * @author fernando
  */
-public class CropControlControl {
+public class CropControl {
 
     // constants
     private static final int LAND_BASE = 17; 
     private static final int LAND_RANGE = 10;
+    private static final int POPULATION_ALLOW_PER_ACRE = 10;
     // random number generator
     private static Random random = new Random();
 
@@ -63,4 +64,46 @@ public class CropControlControl {
         //return acresOwned
         return acresOwned; // Value storage or display it to the calling method
     }
+    
+/**    
+ * The function is calculating the total amount of acres owned. It will have four inputs (acresToBuy, landPrice, acresOwned, wheatInStore) 
+ * and it will return two values; the amount of acresOwned and wheatInStore after the purchase. 
+ * The following validation rules need to be enforced: 
+ * The number of acres of land to buy must be greater than 0. 
+ * The city population divided by 10 must be equal or greater than number of acres to buy.
+ * The Wheat In Store must be equal or greater than the Price of Land.
+ * The Price of the land must be equal or greater than 17 and less or equal to 27.
+ */
+    
+    public static int buyLand(int landPrice, int acresToBuy, CropData cropData) {
+        //If acresToBuy < 1, return -1
+         if(acresToBuy < 1)
+             return -1;
+             
+        //If acresToBuy > (City population / 10), returns -1
+        int population = cropData.getPopulation();
+        if(acresToBuy > (population / POPULATION_ALLOW_PER_ACRE))
+            return -1;
+       
+        //If wheatInStore < (acresToBuy * landPrice), returns -1
+        int finalPrice = acresToBuy * landPrice;
+        int wheatInStore = cropData.getWheatInStore();
+        if(wheatInStore < finalPrice)
+            return -1;
+        
+        //acresOwned = acresOwned + acresToBuy
+        int acresOwned = cropData.getAcresOwned();
+        acresOwned += acresToBuy;
+        cropData.setAcresOwned(acresOwned);
+        
+        //wheatInStore = wheatInStore - (acresToBuy * landPrice)
+        wheatInStore -= finalPrice;
+        cropData.setWheatInStore(wheatInStore);
+        
+        //returns acresOwned
+        return acresOwned;
+        
+    }
+            
+    
 }

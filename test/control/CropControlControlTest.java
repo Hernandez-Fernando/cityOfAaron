8a08a8c1a33de4d3526a1a8be1ab52718d496225
Fernando Hernandez <her16028@byui.edu>
2018-10-24 10:51:58 -0700

    Finished Intro to Testing File
    Added all the test matrixes to the file

--- test/control/CropControlControlTest.java
+++ test/control/CropControlControlTest.java
@@ -27,24 +27,93 @@ public class CropControlControlTest {
         int expResult = 0;
         int result = CropControlControl.calcLandCost();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
     /**
      * Test of sellLand method, of class CropControlControl.
+     * Test Matrix 1
      */
     @Test
-    public void testSellLand() {
-        System.out.println("sellLand");
-        int landPrice = 0;
-        int acresToSell = 0;
-        CropData cropData = null;
+    public void testSellLandCase1() {
+        
+        System.out.println("sellLand - Test  Case 1");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(2800);
+        
+        int landPrice = 20;
+        int acresToSell = 10;
+        int expResult = 2790;
+        int result = CropControlControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of sellLand method, of class CropControlControl.
+     * Test Matrix 2
+     */
+    @Test
+    public void testSellLandCase2() {
+        System.out.println("sellLand - Test Case 2");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(2800);
+        
+        int landPrice = 20;
+        int acresToSell = -5;
+        int expResult = -1;
+        int result = CropControlControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of sellLand method, of class CropControlControl.
+     * Text Matrix 3
+     */
+    @Test
+    public void testSellLandCase3() {
+        System.out.println("sellLand - Test Case 3");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(2800);
+        
+        int landPrice = 20;
+        int acresToSell = 3000;
+        int expResult = -1;
+        int result = CropControlControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of sellLand method, of class CropControlControl.
+     * Text Matrix 4
+     */
+    @Test
+    public void testSellLandCase4() {
+        System.out.println("sellLand - Test Case 4");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(2000);
+        
+        int landPrice = 10;
+        int acresToSell = 2000;
         int expResult = 0;
         int result = CropControlControl.sellLand(landPrice, acresToSell, cropData);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
+    
+    /**
+     * Test of sellLand method, of class CropControlControl.
+     * Text Matrix 5
+     */
+    @Test
+    public void testSellLandCase5() {
+        System.out.println("sellLand - Test Case 5");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(2000);
+        
+        int landPrice = 10;
+        int acresToSell = 0;
+        int expResult = 2000;
+        int result = CropControlControl.sellLand(landPrice, acresToSell, cropData);
+        assertEquals(expResult, result);
+    }
+
     
 }

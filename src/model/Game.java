b1e5b81f28c98e8f018f209eb0db07acbbd25ea2
Sara Campoverde de Burgos <sasucamp@hotmail.com>
2018-11-08 03:40:48 -0500

    W07 added Game CONTROL | Annika

--- src/model/Game.java
+++ src/model/Game.java
@@ -34,7 +34,7 @@ public class Game implements Serializable {
     }
 
     public void setPlayer(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
    
    

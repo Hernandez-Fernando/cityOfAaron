05ffe3cabf187c6e7245d0aef53b647f03e18f53
Fernando Hernandez <her16028@byui.edu>
2018-11-08 14:35:03 -0700

    Main Class Updated
    Deleted old test code in the main class that is not needed according to the W07 Team Assignment feedback.

--- src/cityofaaron/CityOfAaron.java
+++ src/cityofaaron/CityOfAaron.java
@@ -26,70 +26,7 @@ public class CityOfAaron {
         
         // runs the main menu
         mmv.displayMenu(); 
-        
-        // game object
-        Game myGame = new Game();
-
-        // Player object
-        Player myPlayer = new Player();
-        
-        //Set player name
-        myPlayer.setName("Charlie");
-        
-        
-
-        System.out.println("********** Player Class Testing ********");
-        String myPlayerName = myPlayer.getName();
-        System.out.println("Name = " + myPlayerName);
-
-        String memberTwoName = TeamMembers.Member2.getName();
-        String memberTwoTitle = TeamMembers.Member2.getTitle();
-
-        System.out.println("\n\n********** TeamMembers Class Testing ********");
-        System.out.println("Member Name = " + memberTwoName);
-        System.out.println("Member Title = " + memberTwoTitle);
-
-
-        //ListItem Test        
-        String itemOneName = ListItem.ItemOne.getName();
-        int itemOneNumber = ListItem.ItemOne.getNumber();
-
-        System.out.println("\n\n********** ListItem Class Testing ********");
-        System.out.println("Item Name = " + itemOneName);
-        System.out.println("Item Number = " + itemOneNumber);
-
-        //CropData Test
-        CropData theCropData = new CropData();
-        theCropData.setYear(1993);
-        theCropData.setPopulation(12464);
-        theCropData.setAcresOwned(12);
-        theCropData.setCropYield(16);
-        theCropData.setWheatInStore(123556);
-        theCropData.setNumberWhoDied(19);
-        theCropData.setNewPeople(47);
-        theCropData.setHarvest(13);
-        theCropData.setHarvestAfterOffering(3);
-        theCropData.setOffering(10);
-        theCropData.setOfferingBushels(193);
-        theCropData.setPeopleFed(1345);
-        theCropData.setAcresPlanted(90);
-        theCropData.setNumStarved(1335);
-        theCropData.setEatenByRats(63);
-        theCropData.setWheatForPeople(1243);
-
-        System.out.println("\n\n********** CropData Class Testing ********");
-        System.out.println(theCropData.toString());
-
-        // Location Testing
-        Location theLocation = new Location();
-        //theLocation.setDescription("This is a game");
-        //theLocation.setSymbol("Shield");
-
-        System.out.println("\n\n********** Location Class Testing ********");
-        System.out.println(theLocation.toString());
-        
-       } 
-    
+    }
     /**
      * Purpose: get the game
      * @return the game instance
